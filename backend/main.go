fd4dcaa2b33599fca216d719f1f6e784bdd03ef2
jjohnston55 <46493038+jjohnston55@users.noreply.github.com>
2020-09-22 12:36:12 -0600

    Can now handle multiple clients

--- backend/main.go
+++ backend/main.go
@@ -7,19 +7,29 @@ import (
 	"./pkg/websocket"
 )
 
-func servWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := websocket.Upgrade(w, r)
+func servWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", servWs)
+	pool := websocket.NewPool()
+
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		servWs(pool, w, r)
+	})
 }
 
 func main() {
