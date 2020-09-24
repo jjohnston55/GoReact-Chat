b312e6bdefd71904a103d3007fa87fbd7cb764ee
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-09-24 14:59:41 -0600

    Adjusted size for large screens

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -5,8 +5,12 @@ const Header = () => {
 	return (
 		<div className='header'>			
 			<h1>GoReact Chatroom</h1>
-			<img id='golang' src='./img/gopher.png' alt='gopher.png' title='Golang Gopher' />
-			<img id='react' src='./img/react.png' alt='react.png' />
+			<a href='https://golang.org/' target="_blank" rel="noopener noreferrer">
+				<img id='golang' src='./img/gopher.png' alt='gopher.png' />
+			</a>
+			<a href='https://reactjs.org/' target="_blank" rel="noopener noreferrer">
+				<img id='react' src='./img/react.png' alt='react.png' />
+			</a>
 		</div>
 	)
 }
